3a08a3247f179f27b770a0e6163ed4bb5608c452
Teddy Astie <teddy.astie@outlook.fr>
2024-02-15 14:08:00 +0100

    Fix identity domain allocation, implement device probe, device groups, map/unmap, fix init.

--- drivers/iommu/xen-iommu.c
+++ drivers/iommu/xen-iommu.c
@@ -2,6 +2,9 @@
  * Xen driver for the paravirtualized IOMMU
  *
  */
+
+#define dev_fmt(fmt)	"xen-iommu: " fmt
+
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/types.h>
@@ -14,13 +17,21 @@
 #include <asm/string.h>
 #include <linux/device/driver.h>
 #include <linux/slab.h>
+#include <linux/err.h>
+#include <linux/printk.h>
+#include <linux/stddef.h>
 
 #include <xen/xen.h>
 #include <xen/page.h>
+#include <xen/interface/physdev.h>
 #include <xen/interface/pv-iommu.h>
 #include <asm/xen/hypercall.h>
 
 //#include "dma-iommu.h"
+
+MODULE_DESCRIPTION("Xen IOMMU driver");
+MODULE_AUTHOR("Teddy Astie <teddy.astie@vates.tech>");
+MODULE_LICENSE("GPL");
 
 #define MSI_RANGE_START         (0xfee00000)
 #define MSI_RANGE_END           (0xfeefffff)
@@ -29,38 +40,27 @@
 
 #define MAX_REQS   0x8000
 
-struct xen_iommu {
-    struct iommu_device device;
-};
-
 struct xen_iommu_domain {
     struct iommu_domain domain;
     
     u16 ctx_no;
 };
 
-static struct xen_iommu xen_iommu_device;
+static struct iommu_device xen_iommu_device;
 
-static int xen_iommu_attach_dev(struct iommu_domain *domain, struct device *dev)
+static inline struct xen_iommu_domain *to_xen_iommu_domain(struct iommu_domain *dom)
 {
-    if (!dev_is_pci(dev))
-        return -EINVAL;
-
-    /* TODO */
-    return 0;
+    return container_of(dom, struct xen_iommu_domain, domain);
 }
 
-struct iommu_domain_ops xen_iommu_domain_ops = {
-    .attach_dev = xen_iommu_attach_dev,
-};
 
-static bool xen_iommu_capable(struct device *dev, enum iommu_cap cap)
+bool xen_iommu_capable(struct device *dev, enum iommu_cap cap)
 {
     /* No specific iommu_cap */
     return false;
 }
 
-static struct iommu_domain *xen_iommu_domain_alloc(unsigned type)
+struct iommu_domain *xen_iommu_domain_alloc(unsigned type)
 {
     struct xen_iommu_domain *domain;
     int ret;
@@ -71,13 +71,15 @@ static struct iommu_domain *xen_iommu_domain_alloc(unsigned type)
         .subop_id = IOMMUOP_alloc_context
     };
     
-    if (!(type & IOMMU_DOMAIN_IDENTITY))
+    if (type & IOMMU_DOMAIN_IDENTITY)
         op.flags |= IOMMU_CREATE_clone;
 
     ret = HYPERVISOR_iommu_op(&op, 1);
 
-    if (!ret)
-        return NULL;
+    if (ret) {
+        pr_err("Unable to create Xen IOMMU context (%d)", ret);
+        return ERR_PTR(ret);
+    }
 
     domain = kzalloc(sizeof(*domain), GFP_KERNEL);
 
@@ -90,13 +92,153 @@ static struct iommu_domain *xen_iommu_domain_alloc(unsigned type)
     return &domain->domain;
 }
 
-struct iommu_ops xen_iommu_ops = {
+struct iommu_group *xen_iommu_device_group(struct device *dev)
+{
+	if (!dev_is_pci(dev))
+		return ERR_PTR(-ENODEV);
+
+	return pci_device_group(dev);
+}
+
+struct iommu_device *xen_iommu_probe_device(struct device *dev)
+{
+    if (!dev_is_pci(dev))
+        return ERR_PTR(-ENODEV);
+
+    return &xen_iommu_device;
+}
+
+void xen_iommu_release_device(struct device *dev)
+{
+    int ret;
+    struct pv_iommu_op op = {
+        .subop_id = IOMMUOP_reattach_device,
+        .flags = 0,
+        .ctx_no = 0
+    };
+
+    if (!dev_is_pci(dev))
+        return;
+
+    struct pci_dev *pdev = to_pci_dev(dev);
+
+    op.reattach_device.dev.seg = pci_domain_nr(pdev->bus);
+    op.reattach_device.dev.bus = pdev->bus->number;
+    op.reattach_device.dev.devfn = pdev->devfn;
+
+    ret = HYPERVISOR_iommu_op(&op, 1);
+
+    if (ret) {
+        pr_warn("Unable to release device %p", &op.reattach_device.dev);
+    }
+}
+
+int xen_iommu_map_pages(struct iommu_domain *domain, unsigned long iova,
+                        phys_addr_t paddr, size_t pgsize, size_t pgcount,
+			            int prot, gfp_t gfp, size_t *mapped)
+{
+    /* TODO: better handling, batching, ... */
+    size_t count = (pgsize / 0x1000) * pgcount;
+    size_t i;
+    struct xen_iommu_domain *dom = to_xen_iommu_domain(domain);
+    struct pv_iommu_op op = {
+        .subop_id = IOMMUOP_map_page,
+        .flags = 0,
+        .ctx_no = dom->ctx_no
+    };
+
+    if (prot & IOMMU_READ)
+        op.flags |= IOMMU_OP_readable;
+
+    if (prot & IOMMU_WRITE)
+        op.flags |= IOMMU_OP_writeable;
+
+    for (i = 0; i < count; ++i) {
+        op.map_page.gfn = paddr >> XEN_PAGE_SHIFT;
+        op.map_page.dfn = iova >> XEN_PAGE_SHIFT;
+
+        int ret = HYPERVISOR_iommu_op(&op, 1);
+
+        if (ret) {
+            pr_err("Map operation failed for context %hu (%d)", dom->ctx_no, ret);
+        } else if (mapped) {
+            *mapped += 1;
+        }
+    }
+
+    return 0;
+}
+
+size_t xen_iommu_unmap_pages(struct iommu_domain *domain, unsigned long iova,
+			                 size_t pgsize, size_t pgcount,
+			                 struct iommu_iotlb_gather *iotlb_gather)
+{
+    /* TODO: better handling, batching, ... */
+    size_t count = (pgsize / 0x1000) * pgcount;
+    size_t i, unmapped;
+    struct xen_iommu_domain *dom = to_xen_iommu_domain(domain);
+    struct pv_iommu_op op = {
+        .subop_id = IOMMUOP_unmap_page,
+        .ctx_no = dom->ctx_no,
+        .flags = 0,
+    };
+
+    for (i = 0; i < count; ++i) {
+        op.unmap_page.dfn = iova >> XEN_PAGE_SHIFT;
+
+        int ret = HYPERVISOR_iommu_op(&op, 1);
+
+        if (ret) {
+            pr_err("Unmap operation failed for context %hu (%d)", dom->ctx_no, ret);
+        } else {
+            unmapped++;
+        }
+    }
+
+    return unmapped;
+}
+
+int xen_iommu_attach_dev(struct iommu_domain *domain, struct device *dev)
+{
+    int ret;
+    struct pci_dev *pdev;
+    struct xen_iommu_domain *dom = to_xen_iommu_domain(domain);
+    struct pv_iommu_op op = {
+        .subop_id = IOMMUOP_reattach_device,
+        .flags = 0,
+        .ctx_no = dom->ctx_no,
+    };
+
+    if (!dev_is_pci(dev))
+        return -EINVAL;
+
+    pdev = to_pci_dev(dev);
+
+    op.reattach_device.dev.seg = pci_domain_nr(pdev->bus);
+    op.reattach_device.dev.bus = pdev->bus->number;
+    op.reattach_device.dev.devfn = pdev->devfn;
+
+    return HYPERVISOR_iommu_op(&op, 1);
+}
+
+static struct iommu_ops xen_iommu_ops = {
     .capable = xen_iommu_capable,
     .domain_alloc = xen_iommu_domain_alloc,
+    .probe_device = xen_iommu_probe_device,
+    .device_group = xen_iommu_device_group,
+    .release_device = xen_iommu_release_device,
+    .pgsize_bitmap = XEN_IOMMU_PGSIZES,
+    .default_domain_ops = &(const struct iommu_domain_ops) {
+        .map_pages = xen_iommu_map_pages,
+        .unmap_pages = xen_iommu_unmap_pages,
+        .attach_dev = xen_iommu_attach_dev,
+    },
 };
 
-static int __init xen_iommu_init(void)
+int __init xen_iommu_init(void)
 {
+    int ret;
+
 	if (!xen_domain())
 		return -ENODEV;
 
@@ -105,23 +247,32 @@ static int __init xen_iommu_init(void)
 
 	pr_info("Initialising Xen IOMMU driver\n");
 
-    memset(&xen_iommu_device, 0, sizeof(xen_iommu_device));
-    xen_iommu_device.device.ops = &xen_iommu_ops;
+    xen_iommu_domain_alloc(0);
 
-    return iommu_device_register(&xen_iommu_device.device, &xen_iommu_ops, NULL);
+    ret = iommu_device_sysfs_add(&xen_iommu_device, NULL, NULL, "xen-iommu");
+    if (ret) {
+        pr_err("Unable to add Xen IOMMU sysfs");
+        return ret;
+    }
+
+    ret = iommu_device_register(&xen_iommu_device, &xen_iommu_ops, NULL);
+    if (ret) {
+        pr_err("Unable to register Xen IOMMU device %d\n", ret);
+        iommu_device_sysfs_remove(&xen_iommu_device);
+        return ret;
+    }
+
+    return 0;
 }
 
-static void __exit xen_iommu_fini(void)
+void __exit xen_iommu_fini(void)
 {
 	pr_info("Unregistering Xen IOMMU driver\n");
 
     /* TOOD: Cleanup ? */
-    iommu_device_unregister(&xen_iommu_device.device);
+    iommu_device_unregister(&xen_iommu_device);
+    iommu_device_sysfs_remove(&xen_iommu_device);
 }
 
 module_init(xen_iommu_init);
 module_exit(xen_iommu_fini);
-
-MODULE_DESCRIPTION("Xen IOMMU driver");
-MODULE_AUTHOR("Teddy Astie <teddy.astie@vates.tech>");
-MODULE_LICENSE("GPL");
